a11d2fa5490a0e31d90306d4cdd26ede516f0fcd
DevSirq <joaosirqueirasantos@gmail.com>
2025-07-01 10:57:18 -0300

    Exibindo as tarefas

--- app/tasks/index.jsx
+++ app/tasks/index.jsx
@@ -1,11 +1,19 @@
 import { View, Text } from "react-native";
+import TaskItem from "../../components/TaskItem";
 
-export default function Tasks () {
-    return(
+export default function Tasks() {
+    return (
         <View>
             <Text>
                 Página para listar tarefas
             </Text>
+            <TaskItem
+                completed
+                text='Estudar React'
+            />
+            <TaskItem
+                text='Estudar React Native'
+            />
         </View>
     )
 }
