c2e3ce8e1f52de385f5663a3b7da813be663c24e
DevSirq <joaosirqueirasantos@gmail.com>
2025-07-02 09:54:28 -0300

    Implemetando o formulário (add-task)

--- app/add-task/index.jsx
+++ app/add-task/index.jsx
@@ -1,11 +1,24 @@
-import { SafeAreaView, Text } from "react-native";
+import { Pressable, SafeAreaView, Text, TextInput, View } from "react-native";
+import { IconSave } from "../../components/Icons";
 
 export default function AddTask() {
     return (
         <SafeAreaView>
             <Text>
-                Adicionar uma tarefa
+                Adicionar uma tarefa:
             </Text>
+            <View>
+                <Text>
+                    Em que você está trabalhando?
+                </Text>
+                <TextInput />
+                <Pressable>
+                    <IconSave />
+                    <Text>
+                        Salvar
+                    </Text>
+                </Pressable>
+            </View>
         </SafeAreaView>
     );
 }
